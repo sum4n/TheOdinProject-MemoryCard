c2a3dde472f62491905a3558036c5f8ed496f499
Suman Das <sum4ndas@gmail.com>
2023-10-01 10:43:35 +0530

    Add highest scoring player name

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,16 @@ import CardContainer from "./components/CardContainer";
 function App() {
   const [currentScore, setCurrentScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
+  const [playerName, setPlayerName] = useState("Agadran");
 
   return (
     <>
       <h1>Project: Memory Card</h1>
       <div className="scores">
         <p className="current-score">Score: {currentScore}</p>
-        <p className="hight-score">High score: {highScore}</p>
+        <p className="hight-score">
+          High score: {highScore} ({playerName})
+        </p>
       </div>
       {/* TODO: Card Container */}
       <CardContainer />
