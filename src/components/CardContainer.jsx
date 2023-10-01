a697a2c4e4d5aaeb727f055369eb7ab8f2e97f7b
Suman Das <sum4ndas@gmail.com>
2023-10-01 10:02:37 +0530

    Add CardContainer

---
+++ src/components/CardContainer.jsx
@@ -0,0 +1,9 @@
+function CardContainer() {
+  return (
+    <div className="card-container">
+      <div className="cards">{/* TODO: Card component */}</div>
+    </div>
+  );
+}
+
+export default CardContainer;
